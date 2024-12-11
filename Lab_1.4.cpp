18e408682d9287be3dbb10e51c50cb224c1f409e
KewRMUTL67 <phonphaijit_ch67@live.rmutl.ac.th>
2024-12-11 20:50:55 +0700

    Lab_1.4.cpp

---
+++ Lab_1.4.cpp
@@ -0,0 +1,52 @@
+/*
+โจทย์
+
+จงเขียนคำสั่งในการสร้างและเรียกใช้งานฟังก์ชัน GetSet() เพื่อรับจำนวนสมาชิกและค่าของแต่ละสมาชิกในเซต จากคีย์บอร์ด ตามโปรโตไทป์ต่อไปนี้ 
+
+FIX CODE
+
+#include <stdio.h>
+
+int GetSet( int [] ) ;
+
+int main() {
+    int *data, num ;
+    num = GetSet( &data ) ;
+    return 0 ;
+}//end function
+TEST CASE
+
+Input
+
+Enter the number of elements: 3
+Enter the elements: 10 20 30
+Output
+
+Number of elements: 3
+10 20 30
+*/
+
+#include <stdio.h>
+
+int GetSet( int *arr[] ) ;
+
+int main() {
+    int *data[ 100 ] , num ;
+    num = GetSet( data ) ;
+    return 0 ;
+}//end function main
+
+int GetSet( int *arr[] ) {
+    int n ;
+    printf( "Enter the number of elements:" ) ;
+    scanf( "%d" , &n ) ;
+    printf( "Enter the elements: " ) ;
+    for ( int i = 0 ; i < n ; i++ ) {
+        scanf( "%d" , &arr[ i ] ) ;
+    } for ( int i = 0 ; i < n ; i++ ) {
+        printf( "%d " , arr[ i ] ) ;
+    }//end for
+    
+    return 0 ;
+    
+}//end function GetSet
