89b2cb99dd433718a77287d5d8ed1230dd1acebf
KewRMUTL67 <phonphaijit_ch67@live.rmutl.ac.th>
2024-12-15 19:06:13 +0700

    HW05-6_Code.cpp

---
+++ HW/HW05/HW05-6/HW05-6_Code.cpp
@@ -0,0 +1,39 @@
+/*
+    จงรับค่าจากผู้ใช้ และวิเคราะห์ว่าจำนวนดังกล่าวเป็นจำนวนเต็มบวก หรือจำนวนเต็มลบ
+    
+    Test case:
+        Input :
+            49
+
+    Output:
+        Positive
+
+    Test case:
+        Input :
+            -90
+
+    Output:
+        Negative
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int input = 0 ;
+
+    printf( "Input :\n" ) ;
+    scanf( "%d" , &input ) ;
+
+    printf( "\nOutput:\n" ) ;
+    if ( input > 0) { 
+        printf( "Positive" ) ;
+    } else if ( input < 0 ) {
+        printf( "Negative" ) ;
+    } else {
+        printf( "Zero" ) ;
+    }//end if ตรวจเช็ก
+    
+    return 0 ;
+    
+}//end function main
