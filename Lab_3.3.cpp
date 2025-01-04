06a6f8e804f9023f94c61420c264c61eadedec39
KewRMUTL67 <phonphaijit_ch67@live.rmutl.ac.th>
2025-01-04 15:40:54 +0700

    Lab_3.3.cpp

--- Lab_3.3.cpp
+++ Lab_3.3.cpp
@@ -35,7 +35,7 @@ struct student {
     float gpa ;
 } ;
 
-struct student upgrade( struct student child ) ;
+struct student upgrade( struct student child ) ;     //Pass by Value
 
 int main() {
     struct student aboy ;
